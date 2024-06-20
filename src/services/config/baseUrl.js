45e248fbe19b3c7796b3968b49b15446b22fd28e
chiennd172002 <chiennd172002@gmail.com>
2024-06-21 01:17:25 +0700

    fix baseURL

--- src/services/config/baseUrl.js
+++ src/services/config/baseUrl.js
@@ -1,7 +1,7 @@
-// const baseUrl = `${process.env.REACT_APP_PROJECT_BASE_URL}`;
+const baseUrl = `${process.env.REACT_APP_PROJECT_BASE_URL}`;
 
 // const baseUrl = `http://localhost:8086/api/v1/`;
-const baseUrl = `http://localhost:8082/api/v1/`;
+// const baseUrl = `http://localhost:8080/api/v1/`;
 // const baseUrl = `https://parameter-store-be-golang.up.railway.app/api/v1/`;
 // const baseUrl = `https://103.166.185.48:6888/api/v1/`;
 // const baseUrl = `https://param-store-be.datn.live/api/v1/`;
